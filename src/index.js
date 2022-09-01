7a0531e0e1847ecfab6fa4f012a59e47413d4d71
Raphael Fusco <raphaelfusco.rar@gmail.com>
2022-09-01 09:10:27 -0300

    Returning values from routes

--- src/index.js
+++ src/index.js
@@ -24,21 +24,42 @@ function checksExistsUserAccount(request, response, next) {
 
 }
 
+function checkExistsTODO(request, response, next) {
+  const userExists = response.locals.user;
+
+  const TODO = userExists.todos.find(todo => todo.id === request.params.id);
+
+  if (TODO) {
+    response.locals.todo = TODO;
+    next();
+  } else {
+    response.status(404).json({error: "TODO doesn't exist!"});
+  }
+}
+
 app.post('/users', (request, response) => {
-  users.push({
+  const userExists = users.find(user => request.body.username === user.username);
+
+  if(userExists) {
+    response.status(400).json({error: 'User already exists!'})
+  };
+
+  const newUser = {
     id: uuidv4(),
     name: request.body.name,
     username: request.body.username,
     todos: []
-  });
+  };
 
-  response.sendStatus(201);
+  users.push(newUser);
+
+  response.status(201).json(newUser);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   const userTODO = response.locals.user.todos;
 
-  response.json(userTODO);
+  response.send(userTODO);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
@@ -59,47 +80,52 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
   };
   users[userIndex] = user;
 
-  response.sendStatus(201);
+  response.status(201).json(newTODO);
 
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
+app.put('/todos/:id', checksExistsUserAccount, checkExistsTODO, (request, response) => {
   const userExists = response.locals.user;
   const userIndex = response.locals.userIndex;
+  const updateTODO = response.locals.todo;
+
+
+  updateTODO.title = request.body.title;
+  updateTODO.deadline = request.body.deadline;
 
   users[userIndex].todos = userExists.todos.map(todo => {
     if(todo.id === request.params.id){
-      return {
-        ...todo,
-        title: request.body.title,
-        deadline: new Date(request.body.deadline)
-      }
+      return updateTODO
     }
     return todo
   })
-
-  response.sendStatus(200);
+  
+  response.status(200).json({
+    title: updateTODO.title,
+    deadline: updateTODO.deadline,
+    done: updateTODO.done
+  });
 
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
+app.patch('/todos/:id/done', checksExistsUserAccount, checkExistsTODO, (request, response) => {
   const userExists = response.locals.user;
   const userIndex = response.locals.userIndex;
+  const updateTODO = response.locals.todo;
+
+  updateTODO.done = true;
 
   users[userIndex].todos = userExists.todos.map(todo => {
     if(todo.id === request.params.id) {
-      return {
-        ...todo,
-        done: true
-      }
+      return updateTODO
     }
     return todo
   });
 
-  response.sendStatus(200);
+  response.status(200).json(updateTODO);
 });
 
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+app.delete('/todos/:id', checksExistsUserAccount, checkExistsTODO, (request, response) => {
   const userExists = response.locals.user;
   const userIndex = response.locals.userIndex;
   
